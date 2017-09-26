41a5fb2ae602938f54702cefd78c6b7d8a4d5d53
Sandeep Kotha <akotha31@gmail.com>
2017-09-26 15:27:18 +0100

    update java installer

--- lib/setup_env.sh
+++ lib/setup_env.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 sudo su
+sudo add-apt-repository -y ppa:webupd8team/java 
+sudo apt-get -y update 
+sudo apt-get -y install oracle-java8-installer oracle-java8-set-default
 wget -P /tmp https://bintray.com/artifact/download/vertx/downloads/vert.x-3.4.2-full.tar.gz
 tar -xzf /tmp/vert.x-3.4.2-full.tar.gz -C /opt/
 export PATH=$PATH:/opt/vertx/bin
