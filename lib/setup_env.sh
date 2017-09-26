a5e4c3035129ae86216418adc3c6540d7f26b6da
Sandeep Kotha <akotha31@gmail.com>
2017-09-26 15:38:46 +0100

    update java installations

--- lib/setup_env.sh
+++ lib/setup_env.sh
@@ -2,7 +2,9 @@
 
 sudo su
 sudo add-apt-repository -y ppa:webupd8team/java 
-sudo apt-get -y update 
+sudo apt-get -y update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer oracle-java8-set-default
 wget -P /tmp https://bintray.com/artifact/download/vertx/downloads/vert.x-3.4.2-full.tar.gz
 tar -xzf /tmp/vert.x-3.4.2-full.tar.gz -C /opt/
